d1753ad61676a9e02c33d0ac040df199cebaa885
Ch3did <kphch3did@gmail.com>
2025-01-14 17:22:42 -0300

    feat: created especific serializers for each route

--- api/serializers/alocacao_serializer.py
+++ api/serializers/alocacao_serializer.py
@@ -7,3 +7,20 @@ class AlocacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Alocacao
         fields = "__all__"
+
+
+class AlocacaoSerializerCreate(serializers.Serializer):
+    projeto_id = serializers.IntegerField(write_only=True, required=True)
+    programadores_id = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=True
+    )
+    horas = alocacao_id = serializers.IntegerField(required=False)
+
+
+class AlocacaoSerializerUpdate(serializers.Serializer):
+    alocacao_id = serializers.IntegerField(write_only=True, required=True)
+    projeto_id = serializers.IntegerField(write_only=True, required=True)
+    programadores_id = serializers.ListField(
+        child=serializers.IntegerField(), write_only=True, required=True
+    )
+    horas = alocacao_id = serializers.IntegerField(required=False)
