6ae9033d1c179ec449a90c719ed546001b8bae2b
Ch3did <kphch3did@gmail.com>
2025-01-14 17:33:37 -0300

    fix: remove wrong import

--- verzel_alocator/views.py
+++ verzel_alocator/views.py
@@ -1,5 +1,3 @@
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
 from rest_framework import status, views
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
