5cad8932aedfdd1112d272c3a0b39db42707ee86
Ch3did <kphch3did@gmail.com>
2025-01-15 15:04:21 -0300

    feat: added custom methods to solve hour calculation

--- api/models/projeto.py
+++ api/models/projeto.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models import Sum
 
+from .alocacao import Alocacao
 from .tecnologia import Tecnologia
 
 
@@ -8,7 +10,17 @@ class Projeto(models.Model):
     data_inicial = models.DateField()
     data_final = models.DateField()
     tecnologias = models.ManyToManyField(Tecnologia, related_name="projetos")
-    horas_por_dia = models.IntegerField(default=8)
 
     def __str__(self):
         return self.nome
+
+    def get_horas_alocadas(self) -> int:
+        """Busca entre todas as alocacaoes a quantidade de horas ja usadas no projeto"""
+        total_horas = Alocacao.objects.filter(projeto=self).aggregate(Sum("horas"))
+        return total_horas.get("horas__sum", 0) or 0
+
+    def get_total_horas(self) -> int:
+        """Considera 8 horas tempo maximo por dia"""
+        delta = self.data_final - self.data_inicial
+        total_horas_projeto = delta.days * 8
+        return total_horas_projeto
